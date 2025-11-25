e57f668fc152a7c0d7cb5f21bb360dc644e18e7c
Ritesh Das <riteshdas8652@gmail.com>
2025-11-25 11:34:24 +0530

    Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider} from "react-router-dom"
 import ThumbnailPage from "./components/pages/ThumbnailPage"
 import SignupPage from "./components/auth/SignupPage"
+import LoginPage from "./components/auth/LoginPage"
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -11,6 +12,10 @@ function App() {
     {
       path:"/signup",
       element:<SignupPage/>
+    },
+    {
+      path:"/login",
+      element:<LoginPage/>
     }
   ])
   return (
